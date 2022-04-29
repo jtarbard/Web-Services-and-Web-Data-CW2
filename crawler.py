0dc5df1186912ca66fe58fe896d228c82bfe9568
sc18jt <sc18jt@leeds.ac.uk>
2022-04-29 15:37:37 +0100

    Refactoring

--- crawler.py
+++ crawler.py
@@ -1,50 +1,94 @@
+from copyreg import pickle
+from logging import raiseExceptions
+from operator import contains
 import sys
+import json
 import requests
 from bs4 import BeautifulSoup
 
-def find():
-    """
-    Finds word(s) in the inverted index and returns a list of all
-    pages containing those word(s)
+class InvertedIndex:
 
-    words -- a list of strings to be searched
-    """
-    pass
+    def __init__(self):
+        self.documents = []
+        self.terms = {}
 
-def indexPrint():
-    """
-    Prints the inverted index for a particular word
+    def print(self):
+        pass
 
-    Keyword arguments:
-    word -- single word to be searched for
-    """
-    pass
-
-def load():
-    """Loads the index from file system"""
-    pass
+    def search(self):
+        pass
 
 
-def build(seed):
-    """ 
-    Builds an inverted index saving it to local file index.txt
+class Crawler:
 
-    Keyword arguments:
-    seed -- url origin point for the crawler
-    """
-    pass
+    def __init__(self):
+        self.queue = []
+        self.index = InvertedIndex()
+
+    def build(self, seed):
+        self.crawl(seed)
+
+    def load(self):
+        try:
+            with open("index.pkl", "wb") as f:
+                self.index = pickle.load(f)
+        except Exception as e:
+            raiseExceptions(e)
+            exit()
+
+    def updateIndex(self):
+        try:
+            with open("index.pkl", "wb") as f:
+                pickle.dump(self.index)
+        except Exception as e:
+            raiseExceptions(e)
+            exit()
+
+    def crawl(self, url):
+        try:
+            req = requests.get(url)
+            req.raise_for_status()
+        
+            soup = BeautifulSoup(req.text, "html.parser")
+            for link in soup.find_all("a"):
+                if url in link:
+                    pass
+                elif "www." in link:
+                    pass # Skip external link
+                else:
+                    url+link
+
+
+                if link not in queue:
+                    queue.append(link.get("href"))
+            
+        except Exception as err:
+            # log error
+            pass
 
 
 def main():
     
-    # Commands
-    if sys.argv[1] == "find":
-        pass
-    elif sys.argv[1] == "print":
-        pass
-    elif sys.argv[1] == "load":
-        pass
-    elif sys.argv[1] == "build":
-        pass
-    else:
-        return "Unknown command line argument, must be one of: 'build', 'load', 'print', or 'find'."
+    crawler = Crawler()
+    
+    while(True):
+
+        command = input()
+
+        if command == "find":
+            crawler.index.search()
+        elif command == "print":
+            crawler.index.print()
+        elif command == "load":
+            crawler.load()
+        elif command == "build":
+            seed = input("URL: ")
+            crawler.build(seed)
+        elif command == "exit":
+            exit()
+        else:
+            print("Unknown command, must be one of: 'build', 'load', 'print', 'find', or 'exit'.")
+
+
+if __name__ == "__main__":
+    main()
