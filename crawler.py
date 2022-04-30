6f1f4c2e2eb93738595497f37af284589eecbf33
sc18jt <sc18jt@leeds.ac.uk>
2022-04-30 22:26:34 +0100

    Appears to be crawling links correctly. Taking 20 minutes to do so however.

--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+import asyncio
 import pickle
 import requests
 import string
@@ -59,12 +60,10 @@ class Crawler:
         strippedSeed = strippedSeed.replace("https://", "")
 
         self.index.root = strippedSeed.split("/")[0]
-        print("ROOT:"+self.index.root)
-        print("SEED:"+strippedSeed.replace(self.index.root, ""))
         seed = strippedSeed.replace(self.index.root, "")
 
         try:            
-            req = requests.get("https://"+seed)
+            req = requests.get("http://"+self.index.root+seed)
             req.raise_for_status()
             
         except Exception as e:
@@ -73,9 +72,9 @@ class Crawler:
 
         return seed
 
-    def request(self, url):
+    def soup(self, url):
         try:
-            req = requests.get("https://"+url)
+            req = requests.get(url)
             req.raise_for_status()
             soup = BeautifulSoup(req.text, "html.parser")
 
@@ -85,48 +84,62 @@ class Crawler:
             print("Request failed:", e)
             exit()
 
-    def enqueue(self, soup):
+    async def enqueue(self, soup):
         for link in soup.find_all("a"):
+            link = link["href"]
+
             if "www." in link:
+                continue
+            
+            link = link.split("?")[0]
+
+            if link in self.index.documents:
                 continue
 
             self.index.documents.append(link)
 
-    def parse(self, soup):
-        try:
-            navbar = soup.find(class_="navbar").decompose()
-        except:
-            pass
+    async def parse(self, soup, docId):
 
-        allowed_tags = ["p", "a", "span", "h1", "h2", "h3", "h4", "h5", "h6", "li", "dt", "dd"]
+        allowed_tags = ["p", "a", "span", "h1", "h2", "h3", "h4", "h5", "h6", "li", "dt", "dd", "td"]
         tags = soup.find_all(allowed_tags, recursive=True)
         content = []
 
         for tag in tags:
             if tag.text != "":
                 text = tag.text.translate(str.maketrans("", "", string.punctuation))
+                text = text.translate(str.maketrans("", "", string.digits))
                 text = " ".join(text.split())
                 content.extend(text.split(" "))
+
+        for text in content:
+            if self.index.index.get(text):
+                self.index.index[text][str(docId)] += 1
+            else:
+                self.index.index[text] = {str(docId): 1}
         
-        print(content)
+        
+    async def crawl(self, link, docId, logging):
+        if logging:
+                print(link, end="\n")
+            
+        soup = self.soup("http://"+self.index.root+link)
+        await self.enqueue(soup)
+        # await self.parse(soup, docId) 
 
-
-    def crawl(self, rawSeed, logging=None):
+    async def start(self, rawSeed, logging=None):
 
         self.seed = self.prepSeed(rawSeed)
         self.index.documents.append(self.seed)
+        docId = len(self.index.documents)
         
         for link in self.index.documents:
-            if logging:
-                print(link, end="\r")
-            
+            await self.crawl(link, docId, logging)
             time.sleep(1)
-            
-            soup = self.request("https://"+self.index.root+link)
-            self.parse(soup)
+
+        self.handler.save(self.index)
 
 
-def main():
+async def main():
     
     index = InvertedIndex()
     handler = InvertedIndexHandler()
@@ -135,19 +148,21 @@ def main():
     if len(sys.argv) == 1:
         print("No command, must be one of: 'build', 'load', 'print', or 'find'.")
     elif  sys.argv[1] == "find":
+        index = handler.load()
         index.search()
     elif  sys.argv[1] == "print":
+        index = handler.load()
         index.print()
     elif  sys.argv[1] == "load":
         index = handler.load()
     elif  sys.argv[1] == "build":
         if len(sys.argv) > 3:
-            crawler.crawl(sys.argv[2], sys.argv[3])
+            await crawler.start(sys.argv[2], sys.argv[3])
         else:
-            crawler.crawl(sys.argv[2])
+            await crawler.start(sys.argv[2])
     else:
         print("Unknown command, must be one of: 'build', 'load', 'print', or 'find'.")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
