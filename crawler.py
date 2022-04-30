48e9c7d2f6ed60a03c8affe9edc565b0c234e0ae
sc18jt <sc18jt@leeds.ac.uk>
2022-04-30 17:51:17 +0100

    Refactored class design. Implemented save and load. Parse partially implemented.

--- crawler.py
+++ crawler.py
@@ -1,93 +1,141 @@
-from copyreg import pickle
-from logging import raiseExceptions
-from operator import contains
+import os
 import sys
-import json
+import time
+import pickle
 import requests
+import string
 from bs4 import BeautifulSoup
 
-class InvertedIndex:
+class Index:
 
     def __init__(self):
+        self.root = ""
         self.documents = []
-        self.terms = {}
+        self.index = {}
 
     def print(self):
-        pass
+        print("Directory Root:"+self.root)
+        print(self.documents)
+        print(self.index)
 
     def search(self):
         pass
 
-
-class Crawler:
+class IndexHandler:
 
     def __init__(self):
-        self.queue = []
-        self.index = InvertedIndex()
+        pass
 
-    def build(self, seed):
-        self.crawl(seed)
+    def save(self, index):
+        try:
+            with open("index.pkl", "wb") as f:
+                pickle.dump(index, f)
+
+        except Exception as e:
+            print("Saving index failed:", e)
 
     def load(self):
         try:
-            with open("index.pkl", "wb") as f:
-                self.index = pickle.load(f)
+            with open("index.pkl", "rb") as f:
+                return pickle.load(f)
+
         except Exception as e:
-            raiseExceptions(e)
+            print("Loading index failed:", e)
+
+    def remove(self):
+        if os.path.exists("index.pkl"):
+            os.remove("index.pkl")
+        else:
+            print("Index file could not be deleted as it does not exist.")
+
+class Crawler:
+
+    def __init__(self, index, handler):
+        self.index = index
+        self.handler = handler
+
+    def prepSeed(self, seed):
+        if "https" not in seed and "http" not in seed:
+            seed = "https://"+seed
+
+        try:            
+            req = requests.get(seed)
+            req.raise_for_status()
+            
+        except Exception as e:
+            print("Seed preperation failed:", e) 
             exit()
 
-    def updateIndex(self):
-        try:
-            with open("index.pkl", "wb") as f:
-                pickle.dump(self.index)
-        except Exception as e:
-            raiseExceptions(e)
-            exit()
+        return seed
 
-    def crawl(self, url):
+    def request(self, url):
         try:
             req = requests.get(url)
             req.raise_for_status()
-        
             soup = BeautifulSoup(req.text, "html.parser")
-            for link in soup.find_all("a"):
-                if url in link:
-                    pass
-                elif "www." in link:
-                    pass # Skip external link
-                else:
-                    url+link
+
+            return soup
+
+        except Exception as e:
+            print("Request failed:", e)
+            exit()
 
 
-                if link not in queue:
-                    queue.append(link.get("href"))
-            
-        except Exception as err:
-            # log error
+    def parse(self, soup):
+        try:
+            navbar = soup.find(class_="navbar").decompose()
+        except:
             pass
+
+        allowed_tags = ["p", "a", "span", "h1", "h2", "h3", "h4", "h5", "h6", "li", "dt", "dd"]
+        tags = soup.find_all(allowed_tags, recursive=True)
+        content = []
+
+        for tag in tags:
+            if tag.text != "":
+                text = tag.text.translate(str.maketrans("", "", string.punctuation))
+                text = " ".join(text.split())
+                content.extend(text.split(" "))
+        
+        print(content)
+
+
+    def crawl(self, rawSeed, logging=None):
+
+        self.seed = self.prepSeed(rawSeed)
+        self.index.documents.append(self.seed)
+        
+        for link in self.index.documents:
+            if logging:
+                print(link, end="\r")
+            
+            time.sleep(1)
+            
+            soup = self.request(link)
+            self.parse(soup)
 
 
 def main():
     
-    crawler = Crawler()
-    
-    while(True):
+    index = Index()
+    handler = IndexHandler()
+    crawler = Crawler(index, handler)
 
-        command = input()
-
-        if command == "find":
-            crawler.index.search()
-        elif command == "print":
-            crawler.index.print()
-        elif command == "load":
-            crawler.load()
-        elif command == "build":
-            seed = input("URL: ")
-            crawler.build(seed)
-        elif command == "exit":
-            exit()
+    if len(sys.argv) == 1:
+        print("No command, must be one of: 'build', 'load', 'print', or 'find'.")
+    elif  sys.argv[1] == "find":
+        index.search()
+    elif  sys.argv[1] == "print":
+        index.print()
+    elif  sys.argv[1] == "load":
+        index = handler.load()
+    elif  sys.argv[1] == "build":
+        if len(sys.argv) > 3:
+            crawler.crawl(sys.argv[2], sys.argv[3])
         else:
-            print("Unknown command, must be one of: 'build', 'load', 'print', 'find', or 'exit'.")
+            crawler.crawl(sys.argv[2])
+    else:
+        print("Unknown command, must be one of: 'build', 'load', 'print', or 'find'.")
 
 
 if __name__ == "__main__":
